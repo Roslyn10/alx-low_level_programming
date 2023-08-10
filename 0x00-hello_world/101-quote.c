0b946e6acff0556a349e7082d6131add2febac1a
Roslyn <roslynclewis1004@gmail.com>
2023-08-10 13:50:08 -0700

    and that piece of art is useful' - Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-fwrite(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", 59);
+char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fwrite(message, sizeof(char), sizeof(message) -1, stderr);
 return (1); 
 }
