eb787a2580346cdfc4494912670a0624af641463
Roslyn <roslynclewis1004@gmail.com>
2023-08-10 13:41:53 -0700

    and that piece of art is useful' - Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - Prints quote
  *
- * Return: Always 1 (Success)
+ * Return: Always 1 (Failure)
  */
 int main(void)
 {
-echo("and that piece of art is useful" - Dora Korpar, 2015-10-19,\n");
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", 59);
 return (1); 
 }
