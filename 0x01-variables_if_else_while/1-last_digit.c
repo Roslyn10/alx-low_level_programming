34d42e466725fcfa3e720fc7a333c61a6cb0a936
Roslyn <roslynclewis1004@gmail.com>
2023-08-12 13:19:34 -0700

    Last digit of n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h> 
+#include <time.h>
 /**
- * main - 
+ * main - Assings random number to variable n
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{ 
+	int n;
+
+	srand(time(0));
+	n = rand() -RAND_MAX / 2;
+
+	printf("%d n ",n);
+
+	if (n > 5)
+		printf("n and is greater than 5\n");
+	else if (n == 0)
+		printf("n and is 0\n");
+	else
+		printf("n and is less than 6 and not 0\n");
+
+	return (0);
+}
+
+
