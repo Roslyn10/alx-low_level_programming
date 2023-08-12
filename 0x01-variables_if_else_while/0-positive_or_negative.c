a966c46340be3fc1c22c1bc04c2fdd42debc9170
Roslyn <roslynclewis1004@gmail.com>
2023-08-12 10:21:09 -0700

    Random number; postive/negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,14 +9,24 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
-	n = rand() -RAND_MAX /2
-		printf("%d is", n);
-	if (n>0)
+	n = rand() -RAND_MAX /2;
+
+		printf("The number: %d is", n);
+
+	if (n > 0)
+	{
 		printf("positive\n");
-	else if ( ==0);
-	printf("zero\n");
-	else 
+	}
+	else if (n == 0)
+	{
+	  	printf("zero\n");
+	}
+	else
+       	{
+	
 		printf("negative\n");
+	}
 	return (0); 
 }
