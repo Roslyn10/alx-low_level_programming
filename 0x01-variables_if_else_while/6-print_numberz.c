adb3e19666b62224355714a41438708f1ccb003f
Roslyn <roslynclewis1004@gmail.com>
2023-08-13 05:47:51 -0700

    Prints single digits of base 10 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digit numbers of base 10, in main function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0'
+		if (num < 9 )
+	}
+	putchar('\n');
+
+	return (0);
+}
