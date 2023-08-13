24c110e939d7f56834d2ce53766909b606e29e5a
Roslyn <roslynclewis1004@gmail.com>
2023-08-13 07:06:30 -0700

    Random number; either negative, 0 or positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,14 +13,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	printf("%d", n);
-
+	printf("%d is", n);
+ 
 	if (n > 0)
-		printf("%d is positive\n");
+		printf(" positive\n");
 	else if (n == 0)
-		printf("%d is zero\n");
+		printf("zero\n");
 	else 
-		printf("%d is negatie\n");
+		printf("negatie\n");
 
 	return (0);
 }
