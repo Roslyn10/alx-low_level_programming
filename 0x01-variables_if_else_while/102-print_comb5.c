a1d44512af335e8bf6616af55f20a2d6219abe65
Roslyn <roslynclewis1004@gmail.com>
2023-08-13 12:40:54 -0700

    Prints combinations of two two-digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,11 +10,11 @@ int main(void)
 
 	for (f_digit = 0; f_digit <= 9; f_digit++)
 	{
-		for (s_digit = f_digit +1; s_digit <= 9; s_digit++)
+		for (s_digit = f_digit + 1; s_digit <= 9; s_digit++)
 		{
-			for (t_digit = s_digit +1; t_digit <= 9; t_digit++)
+			for (t_digit = s_digit + 1; t_digit <= 9; t_digit++)
 			{
-				for (fo_digit = t_digit +1; fo_digit <= 9; fo_digit++)
+				for (fo_digit = t_digit + 1; fo_digit <= 9; fo_digit++)
 				{
 					putchar(f_digit + '0');
 					putchar(s_digit + '0');
