0033562f2dcaaad864ae3fc40b7cde57ef9b41f6
Roslyn <roslynclewis1004@gmail.com>
2023-08-13 11:31:20 -0700

    Prints combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,7 +11,9 @@ int main(void)
 	for (first_digit = 0; first_digit < 9; first_digit++)
 	{
 		for (second_digit = first_digit + 1; second_digit <= 9; second_digit++)
-		for (third_digit = second_digit + 1; third_digit <= 9; third_digit++)
+		{
+			for (third_digit = second_digit + 1; third_digit <= 9; third_digit++)
+
 			{
 				putchar(first_digit + '0');
 				putchar(second_digit + '0');
@@ -25,6 +27,7 @@ int main(void)
 			}
 		}
 	}
+
 		putchar('\n');
 
 		return (0);
