17a7ac2437525919286ec3c76ef53a4f79722a45
Roslyn <roslynclewis1004@gmail.com>
2023-08-13 12:24:09 -0700

    Prints combinations of two two-digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 /**
- * main - Prints combinations of two two-digit 
+ * main - Prints combinations of two two-digit
  *
  * Return: Always (0) (Success)
  */
 int main(void)
 {
-	int first_digit, second_digit, third_digit, fourth_digit; 
+	int f_digit, s_digit, t_digit, fo_digit;
 
-	for (first_digit = 0; first_digit < 10; first_digit++)
+	for (f_digit = 0; f_digit <= 9; f_digit++)
 	{
-		for (second_digit = first_digit +1; second_digit <= 9; second_digit++)
+		for (s_digit = f_digit +1; s_digit <= 9; s_digit++)
 		{
-			for (third_digit = second_digit +1; third_digit <= 9; third_digit++)
+			for (t_digit = s_digit +1; t_digit <= 9; t_digit++)
 			{
-				for (fourth_digit = third_digit +1; fourth_digit <= 9; fourth_digit++)
-				{ 
-					putchar(first_digit + '0');
-					putchar(second_digit + '0');
-					putchar(third_digit + '0');
-					putchar(fourth_digit + '0');
+				for (fo_digit = t_digit +1; fo_digit <= 9; fo_digit++)
+				{
+					putchar(f_digit + '0');
+					putchar(s_digit + '0');
+					putchar(t_digit + '0');
+					putchar(fo_digit + '0');
 
-					if (!(first_digit == 6 && second_digit == 7 && third_digit == 8 && fourth_digit == 9))
+					if (!(f_digit == 6 && s_digit == 7 && t_digit == 8 && fo_digit == 9))
 					{
 						putchar(',');
 						putchar(' ');
