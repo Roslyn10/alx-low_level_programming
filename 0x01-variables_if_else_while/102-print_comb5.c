9062e19731f0180ce6eb7b3cb273e4b73b2f285a
Roslyn <roslynclewis1004@gmail.com>
2023-08-14 04:40:35 -0700

    Prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,36 +6,37 @@
  */
 int main(void)
 {
-	int f_digit, s_digit, t_digit, fo_digit;
+{
+    int f_digit, s_digit, t_digit, fo_digit;
 
-	for (f_digit = 0; f_digit <= 9; f_digit++)
-	{
-		for (s_digit = 0; s_digit <= 9; s_digit++)
-		{
-			for (t_digit = 0; t_digit <= 9; t_digit++)
-			{
-				for (fo_digit = 0; fo_digit <= 9; fo_digit++)
-				{
-					if (f_digit * 10 + s_digit < t_digit * 10 + fo_digit)
-					{
-						putchar(f_digit + '0');
-						putchar(s_digit + '0');
-						putchar(' ');
-						putchar(t_digit + '0');
-						putchar(fo_digit + '0');
+    for (f_digit = 0; f_digit <= 9; f_digit++)
+    {
+        for (s_digit = 0; s_digit <= 9; s_digit++)
+        {
+            for (t_digit = 0; t_digit <= 9; t_digit++)
+            {
+                for (fo_digit = 0; fo_digit <= 9; fo_digit++)
+                {
+                    if (f_digit * 10 + s_digit < t_digit * 10 + fo_digit)
+                    {
+                        putchar(f_digit + '0');
+                        putchar(s_digit + '0');
+                        putchar(' ');
+                        putchar(t_digit + '0');
+                        putchar(fo_digit + '0');
 
-						if (!(f_digit == 9 && s_digit == 8 && t_digit == 9 && fo_digit == 9))
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-				}
-			}
-		}
-	}
+                        if (!(f_digit == 9 && s_digit == 8 && t_digit == 9 && fo_digit == 9))
+                        {
+                            putchar(',');
+                            putchar(' ');
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-	putchar('\n');
+    putchar('\n');
 
-	return (0);
+    return (0);
 }
