3c789d9a2e29cd9478a5ef2e38bfc688dac80239
Roslyn <roslynclewis1004@gmail.com>
2023-08-14 01:58:32 -0700

    Prints all possible combinations of two two-digit numbers.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	int f_digit, s_digit, t_digit, fo_digit;
 
-	for (f_digit = 0; f_digit <= 9; f_digit++)
+	for (f_digit = 0; f_digit <= 6; f_digit++)
 	{
-		for (s_digit = f_digit + 1; s_digit <= 9; s_digit++)
+		for (s_digit = f_digit + 1; s_digit <= 7; s_digit++)
 		{
-			for (t_digit = s_digit + 1; t_digit <= 9; t_digit++)
+			for (t_digit = s_digit + 1; t_digit <= 8; t_digit++)
 			{
 				for (fo_digit = t_digit + 1; fo_digit <= 9; fo_digit++)
 				{
