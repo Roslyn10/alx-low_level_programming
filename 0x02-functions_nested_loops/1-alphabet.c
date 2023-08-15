9b9e1278747170cda396d8dc1d08eb9f5b602a9c
Roslyn <roslynclewis1004@gmail.com>
2023-08-15 11:42:18 -0700

    Prints the alphabet in loweer case using _putchar(3)

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_alphabet - Prints the alphabet
- * Description - Prints the alphabet using _putchar, in lowercase 
+ * Description - Prints the alphabet using _putchar, in lowercase
  * Return: void
  */
 void print_alphabet(void)
