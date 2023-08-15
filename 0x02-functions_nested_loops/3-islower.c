aebbf9da0a3fdbf49994c37ecb8b6e937188a1d9
Roslyn <roslynclewis1004@gmail.com>
2023-08-15 13:45:50 -0700

    Checks for lowercase characters(5)

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - Checks for lower case
- * @c - Returns 1
+ * @c:  Returns 1
  * Description - Returns 0 for every other letter
  * Return: 1 for lowercase c and 0 for every other letter
  */
