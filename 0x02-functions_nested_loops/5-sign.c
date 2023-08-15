4e4a153224c6845550cda58c80171741267634c6
Roslyn <roslynclewis1004@gmail.com>
2023-08-15 15:29:34 -0700

    Prints the sign of a number(1)

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - +; - or 0
+ * Description - Prints 1 if positive, 0 if 0 and  if negative
+ * Return: 1 if +, 0 if 0, -1 if-
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar ('-');
+		return (-1);
+	}
+}
