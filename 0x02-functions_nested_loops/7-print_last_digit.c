f252e2e5187bea734d15d060c770ba3c0240aaa6
Roslyn <roslynclewis1004@gmail.com>
2023-08-16 04:15:09 -0700

    Prints the last digit of a number(4)

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,12 +9,12 @@ int print_last_digit(int d)
 {
 	if (d < 0)
 	{
-		_putchar(d * -1);
+		_putchar((d * -1) + '0');
 		return (d);
 	}
 	else
 	{
-		_putchar(d % 10);
+		_putchar(d % 10 + '0');
 		return (d % 10);
 	}
 }
