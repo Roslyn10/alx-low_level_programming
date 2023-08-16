2e6f77d156220426c26ddcb67c92bffd0db99023
Roslyn <roslynclewis1004@gmail.com>
2023-08-16 06:39:31 -0700

    Natural numbers starting from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * void print_to_98 - Prints from n to 98
+ * Description: Prints natural numbers from n to 98
+ * Return: void
+ * @n: Starting point/ First natural number
+ */
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		printf("%d", n);
+		if (n != 98)
+		{
+			printf(", ");
+		}
+		n++;
+	}
+	printf("\n");
+}
