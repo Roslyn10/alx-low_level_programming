0d59fb02559fde1c42d0a5e2f7729d8a65314ae3
Roslyn10 <roslynclewis1004@gmail.com>
2023-08-18 00:39:48 +0200

    Betty

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,17 +2,16 @@
 /**
  * positive_or_negative - Checks whether the given integer is + or -
  * Description - Checks whether any given number is +, - or 0
- * Return: void 
- * /
+ * @i: Given integer
+ * Return: void
+ */
 
 void positive_or_negative(int i)
 {
 	if (i > 0)
 		printf("%d is positive\n", i);
-
 	else if (i == 0)
 		printf("%d is zero\n", i);
-
 	else
 		printf("%d is negative\n", i);
 }
