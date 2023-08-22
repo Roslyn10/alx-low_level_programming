a43aa6e2d476ae22b565eefc208ca615bd15a64b
Roslyn <roslynclewis1004@gmail.com>
2023-08-22 12:38:16 -0700

    Prints n elements of an array(1)

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,14 +9,17 @@
  */
 void print_array(int *a, int n)
 {
+	int i;
 
-	for (int n = 0;  ; i++)
+	for (i = 0; i < n  ; i++)
 	{
-		printf("%d". a[n]);
+		printf("%d", a[i]);
 
 		if (i < n - 1)
 		{
 			printf(", ");
 		}
+
 	}
+	printf("\n");
 }
