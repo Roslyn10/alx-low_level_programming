c719cadf68dffd768098d2c2f00ffb320594b41c
Roslyn <roslynclewis1004@gmail.com>
2023-08-26 08:28:14 -0700

    Prints numbers from 0 to 9(1)

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,16 +3,14 @@
 /**
  * print_numbers - Prints the numbers form 0 to 9
  * Description - Prints numbers for 0 to 9, followed by a new line
- * @o: Given int
- * Return: 0 Always (Success)
  */
 void print_numbers(void)
 {
 	int o;
 
-	if (o = 0; o <= 9; o++)
+	for (o = 0; o <= 9; o++)
 	{
-		_putchar(o);
+		_putchar ('0' + o);
 	}
 	_putchar('\n');
 }
