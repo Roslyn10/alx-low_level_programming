fac2f5521920394045c2fd6d836fb685d97c9c6a
Roslyn <roslynclewis1004@gmail.com>
2023-08-26 08:35:53 -0700

    Prints Numbers fron 0 to 9 followed by a new line(1)

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,11 @@ void print_most_numbers(void)
 {
 	int r;
 
-	for ( r =0; r < 10; r++)
+	for ( r = 0; r < 10; r++)
 	{
 		if ( r != 2 && r != 4)
 		{
-			_putchar(r);
+			_putchar('0' + r);
 		}
 	}
 	_putchar('\n');
