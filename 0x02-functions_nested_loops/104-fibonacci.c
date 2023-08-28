c7be74eca6c0e860aece7a8189ac7fc88cdf5298
Roslyn <roslynclewis1004@gmail.com>
2023-08-28 07:14:57 -0700

    First 98 of the Fib sequence(1)

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,13 +1,37 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *
- *
- *
+ * main - Finds and prints the first 98 numbers in Fib sequence
+ * DEscription - Prints the first 98 numbers in fibo seq, starting with 1 and 2
+ * Return: 0 Always (Success)
  */
 int main(void)
 {
 	int h = 98;
-	unsigned int d = 1, e = 2;
+	unsigned int d = 1;
+	unsigned int e = 2;
+	unsigned int z;
 
-	
+	printf("%lu, %lu, ", d, e);
+
+	int v;
+
+	for (v = 3; v <= 98; v++)
+	{
+		z = d + e;
+
+		printf("%u", z);
+
+		if (v < 98)
+		{
+			printf(",");
+		}
+
+		d = e;
+		e = z;
+	}
+
+	printf("\n");
+
+	return (0);
+}
