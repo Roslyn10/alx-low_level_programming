a8440b08dee8f8ceb5454f154935b7da9767d606
Roslyn <roslynclewis1004@gmail.com>
2023-08-28 15:07:45 -0700

    Copies a memeory area(1)

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,11 +2,20 @@
 /**
  * char *_memcpy - Copies memory area
  * Description - Copies memory area
- *
- *
- *
+ * @dest: Pointer to the memory area 
+ * @src: Source pointer which will be copied 
+ * @n: The number of bytes to be copied 
  * Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int g;
 
+	for (g = 0; g < n; g++)
+	{
+		*src = *dest;
+		src++;
+		dest++;
+	}
+	return (dest);
+}
