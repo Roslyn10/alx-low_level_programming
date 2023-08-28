51ff4cffe623232c753f9e1961396c1e6706d138
Roslyn <roslynclewis1004@gmail.com>
2023-08-28 08:13:08 -0700

    Prints 0 to 14 10 times(2)

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,6 @@
 /**
  * more_numbers - Prints numbers 1 to 14 10times
  * Description - Prints 10 times the numbers 0 to 14, followed by a new line
- * @f: Given int
  * Return: 0 Always (Success)
  */
 void more_numbers(void)
