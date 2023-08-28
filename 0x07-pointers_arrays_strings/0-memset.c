7cf1bea443b376a8638863503af2819db78630a3
Roslyn <roslynclewis1004@gmail.com>
2023-08-28 14:58:24 -0700

    Fills memory with a constant byte(2)

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,13 +2,13 @@
 /**
  * char *_memset - Fills memory with a constant byte
  * Description - Fills the memory with a cinstant byte
- * @n: Number of bytes 
- * @s: Pointer to the memory area 
- * @b: Constant byte 
+ * @n: Number of bytes
+ * @s: Pointer to the memory area
+ * @b: Constant byte
  * Return: s
  */
 char *_memset(char *s, char b, unsigned int n)
-{ 
+{
 	unsigned int o;
 
 	for (o = 0; n < o; n++)
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * char *_memcpy - Copies memory area
+ * Description - Copies memory area
+ *
+ *
+ *
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
