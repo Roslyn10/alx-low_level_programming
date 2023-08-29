4c790abde86a91b505ebaa2fb9bb503894cba719
Roslyn <roslynclewis1004@gmail.com>
2023-08-29 15:24:13 -0700

    Sets the valuse of a pointer to ca char(1)

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - Sets the value of a pointer to a char
+ * Description - Sets the value of a pointer to a char
+ * @s: Double pointer
+ * @to: Character
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * print_diagsums - Prints the sum of two diagonals of a square matrix
+ * Decription - Prints sum of two diagonals of a square matrix of integers
+ *
+ */
+void print_diagsums(int *a, int size)
+{
+
