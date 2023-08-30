0e8d16e4f79289d1cd9ad66487b8aa0143ed646d
Roslyn <roslynclewis1004@gmail.com>
2023-08-30 13:09:46 -0700

    Returns the length of a string(1)

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,16 @@
 /**
  * _strlen_recursion - Returns the length of a string
  * Description - Returns the value of a string
- * Return: 
  */
 int _strlen_recursion(char *s)
 {
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return 1 + _strlen_recursion(s + 1);
+	}
+}
