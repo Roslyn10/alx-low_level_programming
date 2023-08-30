0bcf2f0ec956c3035778d35d4e393fb07ed0f5ae
Roslyn <roslynclewis1004@gmail.com>
2023-08-30 12:17:03 -0700

    Prints a string followed by a new line(1)

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,6 +2,18 @@
 /**
  * _puts_recursion - Prints a string
  * Description - Prints a string, followed by a new line
+ * @s: Pointer
+ * Return: 0 Always (Success)
  */
 void _puts_recursion(char *s)
 {
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+
+	_puts_recursion(s + 1);
+}
