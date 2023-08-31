866caeb391f5529b33cb78025daaf1098279af26
Roslyn <roslynclewis1004@gmail.com>
2023-08-31 12:20:49 -0700

    Returns the value of x raised to the power of y(1)

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,9 +2,24 @@
 /**
  * _pow_recursion - Returns the value of x to the power of y
  * Descrption - Returns the value of x to the power of y
+ * @x: Base number that will be raised to the power of y
+ * @y: Exponent that should be raised by x
  * Return: y if its lower  than 0, and -1 if not
  */
 int _pow_recursion(int x, int y)
 {
-	if (x == x^y)
+	if (y == 0)
+	{
+		return (1);
+	}
 
+	else if (y < 0)
+	{
+		return (-1);
+	}
+
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
