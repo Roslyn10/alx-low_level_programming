6b96e9c2ae0c89e1d3ae30664bfdfa0ee326c8f0
Roslyn <roslynclewis1004@gmail.com>
2023-08-31 16:30:59 -0700

    Checks if a string is a palindrome(2)

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * is_palindrome - Checks if a string is a palindrome
- * Description - Checks if a string is a palindrome or not
- * Return: 1 if it is and 0 if not
+ * stringlength - Checks the length of a string
+ * Description - Checks the length of a string
+ * @s: Given string
+ * Return: 0
  */
 int stringlength(char *s)
 {
@@ -10,8 +11,16 @@ int stringlength(char *s)
 	{
 		return (0);
 	}
-	return (1+ stringlength(s + 1));
+	return (1 + stringlength(s + 1));
 }
+/**
+ * is_palindrome_helper - Checks the charaters of a string
+ * Description - Checks the string and its characters
+ * @s: Given string
+ * @b: Start of the string
+ * @e: End of the string
+ * Return: 1 b and e are the same, and 0 if not
+ */
 
 int is_palindrome_helper(char *s, int b, int e)
 {
@@ -25,11 +34,18 @@ int is_palindrome_helper(char *s, int b, int e)
 		return (0);
 	}
 
-	return is_palindrome_helper(s, b + 1, e - 1);
+	return (is_palindrome_helper(s, b + 1, e - 1));
 }
 
+/**
+ * is_palindrome - Checks if a string is a palindrome
+ * Description - Checks if a string is a palindrome or not
+ * @s: Given string
+ * Return: 1 if it is and 0 if not
+ */
 int is_palindrome(char *s)
 {
 	int length = stringlength(s);
-	return is_palindrome_helper(s, 0, length - 1);
+
+	return (is_palindrome_helper(s, 0, length - 1));
 }
