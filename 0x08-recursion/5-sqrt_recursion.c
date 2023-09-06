e8d54df8941042484e09cc37aaec106b7433958a
Roslyn <roslynclewis1004@gmail.com>
2023-09-06 14:11:49 -0700

    Fixing syntax and gcc

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,19 +1,5 @@
 #include "main.h"
 /**
- * _sqrt_recursion - Returns the natural root of a number
- * Description - Returns the natural root of a number
- * _sqrt_helper - Recursion function
- * Description - Helps function _sqrt_recusrion
- * @n: Given number
- * Return: 0 if n has a natural root, and -1 if not
- */
-
-int _sqrt_recursion(int n)
-{
-	return (_sqrt_helper(n, 0));
-}
-
-/**
  * _sqrt_helper - Recursion function
  * Description - Helps function _sqrt_recusrion
  * Description - Returns the natural root of a number
@@ -38,3 +24,17 @@ int _sqrt_helper(int n, int g)
 	}
 }
 
+/**
+ * _sqrt_recursion - Returns the natural root of a number
+ * Description - Returns the natural root of a number
+ * _sqrt_helper - Recursion function
+ * Description - Helps function _sqrt_recusrion
+ * @n: Given number
+ * Return: 0 if n has a natural root, and -1 if not
+ */
+
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_helper(n, 0));
+}
