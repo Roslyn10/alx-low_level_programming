5b03c149acc20b7434e700be4f30e37de3fdcfde
Roslyn <roslynclewis1004@gmail.com>
2023-09-06 03:23:27 -0700

    Fixing atoi

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -3,7 +3,27 @@
  * _atoi - Converts a string to an integer
  * Description - Converts a string to a given integer
  * @s: Given integer
- * Return: 0 Always (Success)
+ * Return: 0 if no numbers in string or return the integer
  */
 int _atoi(char *s)
 {
+	int g = 1;
+	int d = 0;
+	unsigned int r = 0;
+
+	while (s[d] != '\0' && (s[d] < '0' || s[d] > '9'))
+	{
+		if (s[d] == '-')
+		{
+			g *= -1;
+		}
+		d++;
+	}
+
+	while (s[d] >= '0' && s[d] <= '9')
+	{
+		r = (r * 10) + (s[d] - '0');
+		d++;
+	}
+	return (r * g);
+}
