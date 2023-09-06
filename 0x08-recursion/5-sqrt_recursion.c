002037a38852c521242e4d7a2a324fcf191af629
Roslyn <roslynclewis1004@gmail.com>
2023-09-06 14:06:03 -0700

    Fixing betty

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,20 +1,9 @@
 #include "main.h"
 /**
- * _sqrt_helper - Recursion function
- * Description - Helps function _sqrt_recusrion
- * _sqrt-recursion - Returns the natural root of a number
- * Description - Returns the natural root of a number
- * @n: Given number
- * @g: Placeholder
- * Return: n and 0
- */
-int _sqrt_helper(int n, int g);
-/**
- * _sqrt-recursion - Returns the natural root of a number
+ * _sqrt_recursion - Returns the natural root of a number
  * Description - Returns the natural root of a number
  * _sqrt_helper - Recursion function
  * Description - Helps function _sqrt_recusrion
- * @g: Placeholder
  * @n: Given number
  * Return: 0 if n has a natural root, and -1 if not
  */
@@ -23,6 +12,15 @@ int _sqrt_recursion(int n)
 {
 	return (_sqrt_helper(n, 0));
 }
+
+/**
+ * _sqrt_helper - Recursion function
+ * Description - Helps function _sqrt_recusrion
+ * Description - Returns the natural root of a number
+ * @n: Given number
+ * @g: Placeholder
+ * Return: n and 0
+ */
 
 int _sqrt_helper(int n, int g)
 {
