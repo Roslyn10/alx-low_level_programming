32b9fd6f810c271e4e290c234f0abe0017fae85b
Roslyn <roslynclewis1004@gmail.com>
2023-09-07 04:32:09 -0700

    Allocayed memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,8 +5,18 @@
 /**
  * void *malloc_checked - Allocated memory using malloc
  * Description - Allocates memory using malloc
- * @b:
+ * @b: Number of bytes
  * Return: Pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
+	void *d;
+	
+	d = malloc(b);
+
+	if (d == NULL)
+	{
+		exit(98);
+	}
+	return (d);
+}
