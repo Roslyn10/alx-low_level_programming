3e0d9521f8033d8abdca082e1f96d610689f91eb
Roslyn <roslynclewis1004@gmail.com>
2023-09-07 09:18:31 -0700

    Creates an array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -5,9 +5,33 @@
 /**
  * *array_range - Creates an array of integers
  * Description - Creates an array of integers
- * @min:
- * @max:
+ * @min: Minimum number of intgers
+ * @max: Maximum number of integers
  * Return: NULL if min > max and when malloc fails
  */
 int *array_range(int min, int max)
 {
+	int f;
+	int *l;
+	int ne;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	ne = max - min + 1;
+
+	l = (int *)malloc(ne * sizeof(int));
+	if (l == NULL)
+	{
+		return (NULL);
+	}
+
+	for (f = 0; f < ne; f++)
+	{
+		l[f] = min + f;
+	}
+
+	return (l);
+}
