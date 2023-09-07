7ba9554207f830b098432173e2c5e1b0640a32ef
Roslyn <roslynclewis1004@gmail.com>
2023-09-07 08:41:28 -0700

    Allocates memory for an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -5,9 +5,34 @@
 /**
  * void *_calloc - Allocates memory for an array, using malloc
  * Description - Allocated memory fro an array, using malloc
- * @nmemb:
- * @size:
+ * @nmemb: Number of elemnts in the arry
+ * @size: Size of each element
  * Return: NULL if nmemb or size is 0 and when malloc fails
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	int z;
+	char *k;
+	int t;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	t = nmemb * size;
+
+	k = (char *)malloc(t);
+
+	if (k == NULL)
+	{
+		return (NULL);
+	}
+
+	for (z = 0; z < t; t++)
+	{
+		k[z] = 0;
+	}
+
+	return (k);
+}
