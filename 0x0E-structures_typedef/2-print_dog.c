a8226cbae7ffeadf19843aefb8eae75830282fb9
Roslyn <roslynclewis1004@gmail.com>
2023-09-11 13:53:55 -0700

    Prints struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,8 +5,20 @@
 /**
  * print_dog - Prints a struct dog
  * Description - Prints a poochie
- * @d:
+ * @d: Pointer to struct dog and init_dog
  * Return: nil if any element is NULL and nothing if d is NULL
  */
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+	{
+		printf(" ");
+		return;
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %.1f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+}
