af40cb440f7d1616f06945ad1d53a48f2d18d86b
Roslyn <roslynclewis1004@gmail.com>
2023-09-11 12:47:35 -0700

    Fixing dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,6 @@
-#ifdef DOG_H
-#define DOG_H
-
-#include <stdio.h>
-
-/**
- * my_dog.name - Assiges a name to the dog
- * my_dog.age - Assigns an age to the dog
- * my_do.owner - Assigns an owner to the dog
- */
-
 struct dog
 {
 	char *name;
 	double age;
 	char *owner;
 };
-
-#endif
