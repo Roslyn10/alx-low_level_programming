ff5d40e023df97f5680716b849132cf5fa41f19d
Roslyn <roslynclewis1004@gmail.com>
2023-09-11 12:34:43 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,1 +1,16 @@
-#include 
+#ifdef DOG_H
+#define DOG_H
+/**
+ * my_dog.name - Assiges a name to the dog
+ * my_dog.age - Assigns an age to the dog
+ * my_do.owner - Assigns an owner to the dog
+ */
+
+struct dog
+{
+char *name;
+float *age;
+char *owner;
+}
+
+#endif
