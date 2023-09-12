11232d07abdd290458e0f576e3b4751682d7b207
Roslyn <roslynclewis1004@gmail.com>
2023-09-12 05:47:13 -0700

    SIZE

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+#define SIZE 1024
+/**
+ * SIZE - An abbreviation for 1024
+ * Description - Defines a macro named SIZE as an abbreviation for 1024
+ * Return: Nothing
+ */
+
+#endif
