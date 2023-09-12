fc6e7300bb3f5c136a8e2a63050980c9d209b9aa
Roslyn <roslynclewis1004@gmail.com>
2023-09-12 05:40:04 -0700

    Pi

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,14 @@
+#ifndef PI_H
+#define PI_H
+
+#include <stdio.h>
+
+#define PI 3.14159265359
+
+/**
+ * PI - An abbreviation for 3.14159265359
+ * Description - Defnes a macro named PI as an abbreviation for 3.14159265359
+ * Return: Nothing
+ */
+
+#endif
