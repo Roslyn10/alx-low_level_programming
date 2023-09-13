46205868223a3603ddeadbe9173b0c6f2e306bbb
Roslyn <roslynclewis1004@gmail.com>
2023-09-13 14:19:26 -0700

    Fixing blank output

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdio.h>
-
-/** print_opcodes - Prints the opcodes of its own main function
+/**
+ * print_opcodes - Prints the opcodes of its own main function
  * @size: Number of bytes to be read
  * @start: Pointer to where the opcodes will be read from
  * Return: Nothing
  */
 
-void print_opcodes(unsigned char *start,size_t size)
+void print_opcodes(unsigned char *start, size_t size)
 {
 	size_t n;
 
@@ -28,7 +28,7 @@ void print_opcodes(unsigned char *start,size_t size)
  * @argv: Argument Vector
  * Return: 0 if successful, 1 if not correct and 2 if n_b is negative
  */
- 
+
 int main(int argc, char *argv[])
 {
 	int n_b, r;
@@ -39,23 +39,19 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Error\n");
-		{
-			return (1);
-		}
+		return (1);
 	}
 
-	if (n_b < 0)
+	n_b = atoi(argv[1]);
 	{
-		printf("Error\n");
-		{
-			return (2);
-		}
+		printf("Rrroe\n");
+		return (2);
 	}
 
 	r_s = (size_t)n_b;
 
 	start = (unsigned char *)&main;
-	for (f = 0; f < r_s;f++)
+	for (f = 0; f < r_s; f++)
 	{
 		printf("%02x", (unsigned int)start[f]);
 		if (f < r_s - 1)
