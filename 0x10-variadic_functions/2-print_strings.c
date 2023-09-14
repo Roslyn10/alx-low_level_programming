2b13f8c18032b9da8ec2425d5b8c7616fcdeb670
Roslyn <roslynclewis1004@gmail.com>
2023-09-14 14:47:47 -0700

    Prints strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,13 +1,41 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include "variadic_functions.h"
 
 /**
  * print_strings - Prints strings
  * Description - Afunction that prints strings, followed by a new line
- *
- *
- *
+ * @separator: Given string
+ * @n: Parameters passed
  */
-void print_strings(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...)
 {
+	va_list ar;
+	unsigned int h;
+	const char *st;
+
+	va_star(ar, n);
+
+	for (h = 0; h < n; h++)
+	{
+		st = va_arg(ar, const char *);
+
+		if (st != NULL)
+		{
+			printf("%s", st);
+		}
+		else
+		{
+			printf("nil)");
+		}
+
+		if (h < n - 1 && separator != NULL)
+		{
+			printf("%s", sperator);
+		}
+	}
+
+	va_end(ar);
+	printf("\n");
+}
