16705aaa827675dc3be9c7d58e2418ed9d75e69d
Roslyn <roslynclewis1004@gmail.com>
2023-09-20 09:51:14 -0700

    Fixing the recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,10 +3,34 @@
 #include <stdlib.h>
 
 /**
+ * is_prime_helper - Checks whether an integer is a prime number
+ * Description - Checks whether an int is a prime number
+ * @n: Given integer
+ * @m: Divisor
+ * Return: 0 if its not and 1 if it is
+ */
+
+int is_prime_helper(int n, int m)
+{
+	if (m <= 1)
+	{
+		return (1);
+	}
+
+	if (n % m == 0)
+	{
+		return (0);
+	}
+
+	return (is_prime_helper(n, m - 1));
+}
+
+
+/**
  * is_prime_number - Checks if an integer is a prime number
  * Description - Chaecks if an integer is a prime number or not
  * @n: Given integer
- * Return:
+ * Return: Prime recusrion
  */
 
 int is_prime_number(int n)
@@ -15,28 +39,6 @@ int is_prime_number(int n)
 	{
 		return (0);
 	}
-	return ((n) n - 1);
-}
 
-/**
- * is_prime_helper - Checks whether an integer is a prime numebr or not
- * Description - Checks whethera given integer is a prime number or not
- * @n: Given integer
- * @v: Divisor
- * Return: 0 if its not and 1 if it is
- */
-int is_prime_helper(int n)
-{
-	int v;
-
-	if (v == 1)
-	{
-		return (1);
-	}
-	if (n % v == 0 && v > 0)
-	{
-		return (0);
-	}
-
-	return ((n) v - 1);
+	return (is_prime_helper(n, n / 2));
 }
