61e31e1fc079f9804a8abd15f996c559f6627a8a
Roslyn <roslynclewis1004@gmail.com>
2023-09-22 03:31:53 -0700

    Fixing blank output

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,10 @@
-global	main
-extern printf
+global   main
+	extern    printf
+main: 
+	mov   edi, format
+	xor   eax, eax
+	call  printf
+	mov   eax, 0
+	ret
+format: db `Hello, Holberton\n`,0
 
-	format db 'Hello, Holberton', 0
-main:
-	mov edi, format
-	xor eax,eax
-	call printf
-	mov eax, 0
-	xor edi, edi
-	syscall
