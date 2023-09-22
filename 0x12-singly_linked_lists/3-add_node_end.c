41e551d81f40d75fad897fc976f78a4167838a11
Roslyn <roslynclewis1004@gmail.com>
2023-09-21 21:20:24 -0700

    Adds new nodes

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include "lists.h"
 
@@ -10,4 +11,41 @@
  * Return:
  */
 
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *nd;
+	list_t *current;
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	nd = (list_t *)malloc(sizeof(list_t));
+
+	if (nd == NULL)
+	{
+		return (NULL);
+	}
+
+	nd->str = strdup(str);
+
+	nd->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = nd;
+	}
+	else
+	{
+		current = *head;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+
+		current->next = nd;
+	}
+
+	return (nd);
+}
