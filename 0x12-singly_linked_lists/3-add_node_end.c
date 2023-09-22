3728f8ffb52a523ee98c579b377f7b86b6f29233
Roslyn <roslynclewis1004@gmail.com>
2023-09-22 03:14:46 -0700

    Fixing memory leak

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -5,7 +5,7 @@
 
 /**
  * add_node_end - A function that adds a new node at the end of a list_t list
- * DEscription- Adds a new node at the end of a list_t list
+ * Description- Adds a new node at the end of a list_t list
  * @head: Double poiner
  * @str: Given string
  * Return: Address of the new node or NULL if failed
@@ -14,38 +14,36 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *nd;
-	list_t *current;
+	list_t *t = *head;
+	unsigned int len = 0;
 
-	if (str == NULL)
+	while (str[len])
 	{
-		return (NULL);
+		len++;
 	}
 
-	nd = (list_t *)malloc(sizeof(list_t));
-
+	nd = malloc(sizeof(list_t));
 	if (nd == NULL)
 	{
 		return (NULL);
 	}
 
 	nd->str = strdup(str);
-
+	nd->len = len;
 	nd->next = NULL;
 
 	if (*head == NULL)
 	{
 		*head = nd;
+		return (nd);
 	}
-	else
-	{
-		current = *head;
-		while (current->next != NULL)
-		{
-			current = current->next;
-		}
 
-		current->next = nd;
+	while (t->next)
+	{
+		t = t->next;
 	}
+
+	t->next = nd;
 
 	return (nd);
 }
