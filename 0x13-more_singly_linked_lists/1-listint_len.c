8851619e84fcbfe548d38e0f7419f1758472dce4
Roslyn <roslynclewis1004@gmail.com>
2023-09-25 07:22:46 -0700

    Returns the number of elements

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -5,10 +5,19 @@
 /**
  * listint_len - A function that returns the number os elements in linked list
  * Description - Returns the number of elements in a linked listint_t list
- * @listint_t:
- * @h:
+ * @h: Pointer to head of list
  * Return: The number of elements
  */
 
-size_t listint_len(const listint_t *h
+size_t listint_len(const listint_t *h)
 {
+	size_t r = 0;
+
+	while (h != NULL)
+	{
+		r++;
+		h = h->next;
+	}
+
+	return (r);
+}
