0bc06bff85e45b7e3eee82c8e95006819b8d7052
Roslyn <roslynclewis1004@gmail.com>
2023-09-25 10:08:58 -0700

    Frees node

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -5,9 +5,18 @@
 /**
  * free_listint - A function that frees a list
  * Description - Frees a listint_t list
- * @head:
+ * @head: Pointer to the first node
  * Return: Nothing
  */
 
 void free_listint(listint_t *head)
 {
+	listint_t *d; /**traverses through the list**/
+
+	while (head != NULL)
+	{
+		d = head;
+		head = head->next;
+		free(d);
+	}
+}
