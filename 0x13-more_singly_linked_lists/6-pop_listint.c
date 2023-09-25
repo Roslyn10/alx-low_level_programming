e976e2e8976ac6862106088f38727e76ebd996a2
Roslyn <roslynclewis1004@gmail.com>
2023-09-25 13:44:11 -0700

    Frees the head pointer

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -5,9 +5,23 @@
 /**
  * pop_listint - A function that deletes the head node of a linked list
  * Description - Deletes the head and returns the head node's data (n)
- * @head:
+ * @head: Pointer to a pointer to head of list
  * Return: 0 if linked list is empty
  */
 
 int pop_listint(listint_t **head)
 {
+	listint_t *t;
+	int c;
+
+	if (head == NULL || *head == NULL)
+	{
+		return (0);
+	}
+
+	t = *head;
+	*head = t->next;
+	c = t->n;
+	free(t);
+	return (c);
+}
