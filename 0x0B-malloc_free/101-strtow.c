a007ac5fa79792077dffe5dcf3a545242e01dc1f
Roslyn <roslynclewis1004@gmail.com>
2023-09-26 07:09:10 -0700

    Reallocates memory

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "main.h"
 /**
  * **strtow - Splits a string into words
@@ -7,3 +8,50 @@
  */
 char **strtow(char *str)
 {
+	int w_c = 0;
+	char *t;
+	char **w;
+	int k, l;
+
+	if (str == NULL || strcmp(str, "") == 0)
+	{
+		return (NULL);
+	}
+
+	t = strtok(str, " ");
+	while (t != NULL)
+	{
+		w_c++;
+		t = strtok(NULL, " ");
+	}
+
+	w = (char **)malloc((w_c + 1) * sizeof(char *));
+	if (w == NULL)
+	{
+		return (NULL);
+	}
+
+	t = strtok(str, " ");
+	k = 0;
+
+	while (t != NULL)
+	{
+		w[k] = (char *)malloc((strlen(t) + 1) * sizeof(char));
+		if (w[k] == NULL)
+		{
+			for (l = 0; l < k; l++)
+			{
+				free(w[l]);
+			}
+			free(w);
+			return (NULL);
+		}
+		strcpy(w[k], t);
+		k++;
+		t = strtok(NULL, " ");
+	}
+
+	w[w_c] = NULL;
+
+	return (w);
+}
--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <stdio.h>
 #include "main.h"
 #include <stdlib.h>
@@ -5,10 +6,42 @@
 /**
  * *_realloc - Reallocated a memory block using malloc and free
  * Desription - Reallocates a memory block using malloc and free
- * @ptr:
- * @old_size:
- * @new_size:
+ * @ptr: Pointer to memory that should be rellocted
+ * @old_size: Current allocated memory block
+ * @new_size: Desired size of memory block
  * Return: NULL if new_size is = 0, and ptr != NULL
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	void *n_p;
+	size_t c_s;
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (ptr == NULL)
+	{
+		return (malloc(new_size));
+	}
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+
+	n_p = malloc(new_size);
+	if (n_p == NULL)
+	{
+		return (NULL);
+	}
+
+	c_s = (old_size < new_size) ? old_size : new_size;
+	memcpy(n_p, ptr, c_s);
+
+	free(ptr);
+
+	return (n_p);
+}
