1cf448ea7c3a1d2c33a47584e96d00b05e36c33d
Roslyn <roslynclewis1004@gmail.com>
2023-09-26 00:48:47 -0700

    Sum of data(n)

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -5,9 +5,22 @@
 /**
  * sum_listint - A function that returns the sum of all the data of a list
  * Description - Returns the sum of all the data (n) of a listint_t list
- * @head:
- * Return: 0 if list is empty
+ * @head: Pointer to head of list
+ * Return: 0 if list is empty, or sum of all the data
  */
 
 int sum_listint(listint_t *head)
 {
+	listint_t *w;
+	unsigned int s = 0;
+
+	w = head;
+
+	while (w != NULL)
+	{
+		s += w->n;
+		w = w->next;
+	}
+
+	return (s);
+}
