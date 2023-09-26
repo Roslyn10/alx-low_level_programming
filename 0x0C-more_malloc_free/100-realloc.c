90753a8bf7f1d09d555b00843a9738a75fe007f2
Roslyn <roslynclewis1004@gmail.com>
2023-09-26 09:09:19 -0700

    Trying to fix free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,6 +18,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == 0)
 	{
+		if (ptr)
+		{
+			free(ptr);
+		}
 		return (NULL);
 	}
 
@@ -40,6 +44,5 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	c_s = (size_t)((old_size < new_size) ? old_size : new_size);
 	memcpy(n_p, ptr, c_s);
 
-	free(ptr);
 	return (n_p);
 }
