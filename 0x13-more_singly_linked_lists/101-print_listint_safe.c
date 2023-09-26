3f9a53b611c824906197770cb111e43387ce4c39
Roslyn <roslynclewis1004@gmail.com>
2023-09-26 11:38:54 -0700

    Prints a linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,9 +5,41 @@
 /**
  * print_listint_safe - A function that prints a linked list
  * Description - Printsa listint_t linked list
- * @head:
+ * @head: Pointer to head of list
  * Return: the number of nodes in the list
  */
 
 size_t print_listint_safe(const listint_t *head)
 {
+	size_t g = 0; /**Keep track of nodes**/
+	const listint_t *h = head; /**Current node**/
+	const listint_t *c_l; /**Check loop**/
+
+	while (h != NULL)
+	{
+		g++;
+		h = h->next;
+
+		if (c_l != NULL)
+		{
+			c_l = c_l->next;
+			if (c_l != NULL)
+			{
+				c_l = c_l->next;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		if (h == c_l)
+		{
+			exit(98);
+		}
+
+		printf("[&h] %d\n", h->n);
+	}
+
+	return (g);
+}
