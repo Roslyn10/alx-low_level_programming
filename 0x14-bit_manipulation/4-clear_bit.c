91e5e9cc86afd57da32d912e81fb47107de72b03
Roslyn <roslynclewis1004@gmail.com>
2023-09-28 12:23:08 -0700

    Flips bits

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -5,7 +5,7 @@
  * clear_bit - A function that sets the value of a bit to 0 at a given index
  * Description - Sets the value of a bit to 0 at a given index
  * @n: Pointer to unsigned long int, memort location
- * @index: Represents the position of the bit 
+ * @index: Represents the position of the bit
  * Return: 1 if it works, -1 if an error occurs
  */
 
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -6,10 +6,23 @@
  * .. flip to get from one number to another
  * Description: Returns the number of bits needed to flip to get fom one number
  * to another
- * @n:
- * @m:
- * Return:
+ * @n: Input of first given number
+ * @m: Inout of second given number
+ * Return: The bits that were flipped
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	/**Calculates the XOR of n and m**/
+	unsigned long int r = n ^ m;
+	unsigned int btf = 0;
+
+	/**Counts the set bits**/
+	while (r)
+	{
+		btf += r & 1;
+		r >>= 1;
+	}
+
+	return (btf);
+}
