ff82cd8772b857a1f3d3a9c79bf814d5f4eb4183
Roslyn <roslynclewis1004@gmail.com>
2023-10-02 13:34:06 -0700

    Creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * create_file - Creates a file
+ * Description - A function that creates a file
+ * @filename:
+ * @text_cotent:
+ * Return: 1 on success, -1 on failure and when filename is NULL
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd; /**file Descriptor**/
+	ssize_t bwritten;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		bwritten = write(fd, text_content, strlen(text_content));
+		if (bwritten == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
+
