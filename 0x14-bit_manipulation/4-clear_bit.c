4408028596c36617c8055ec6ca526e13d1ef6916
Roslyn <roslynclewis1004@gmail.com>
2023-10-26 13:55:28 -0700

    Changing bits from 8 to 8 -1

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,7 +12,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int bit;
 
-	if (index >= sizeof(unsigned long int) * 8)
+	if (index >= sizeof(unsigned long int) * 8 - 1)
 	{
 		return (0);
 	}
