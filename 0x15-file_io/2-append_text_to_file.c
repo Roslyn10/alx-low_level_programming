e35b08f165d5af52e0fbc33525f7e03ea9b2e329
Roslyn <roslynclewis1004@gmail.com>
2023-10-30 05:24:54 -0700

    Fixing betty

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (1);
 	}
-	
+
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
@@ -29,7 +29,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	bwritten = write (fd,text_content, strlen(text_content));
+	bwritten = write(fd, text_content, strlen(text_content));
 	if (bwritten == -1)
 	{
 		close(fd);
