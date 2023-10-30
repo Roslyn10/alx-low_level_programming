b99a61e76f746a2cc47b817354cd619859c8c905
Roslyn <roslynclewis1004@gmail.com>
2023-10-30 13:45:48 -0700

    Read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -48,4 +48,44 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	free(buffer);
 	fclose(file);
 	return (bread);
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+        FILE *file;
+        char *buffer;
+        size_t bread;
+        ssize_t bwritten;
+
+        if (!filename)
+        {
+                return (-1);
+        }
+        file = fopen(filename, "r");
+        if (!file)
+        {
+                return (-1);
+        }
+        buffer = (char *)malloc(letters + 1);
+        if (!buffer)
+        {
+                fclose(file);
+                return (-1);
+        }
+        bread = fread(buffer, 1, letters, file);
+        if (bread < 0)
+        {
+                free(buffer);
+                fclose(file);
+                return (-1);
+        }
+        buffer[bread] = '\0';
+        bwritten = write(STDOUT_FILENO, buffer, bread);
+        if (bwritten < 0 || (size_t)bwritten != bread)
+        {
+                free(buffer);
+                fclose(file);
+                return (-1);
+        }
+        free(buffer);
+        fclose(file);
+        return (bread);
 }
