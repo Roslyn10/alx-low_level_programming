cbce6f8dd199afd13ac3018b36d82e67f7afdd17
Roslyn <roslynclewis1004@gmail.com>
2023-11-03 08:56:35 -0700

    Adding elf.h

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,44 +10,5 @@
 
 int main(int argc, char *argv[])
 {
-	int fdr, fdw, y, n, o;
-	char buffer[Buffer_Size];
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	fdr = open(argv[1], O_RDONLY);
-	if (fdr < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	fdw = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((y = read(fdr, buffer, Buffer_Size)) > 0)
-	{
-		if (fdw < 0 || write(fdw, buffer, y) != y)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fdr);
-			exit(99);
-		}
-	}
-	if (y < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	n = close(fdr);
-	o = close(fdw);
-	if (n < 0 || o < 0)
-	{
-		if (n < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdr);
-		if (o < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdw);
-			exit(100);
-	}
-	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -18,5 +18,20 @@ int append_text_to_file(const char *filename, char *text_content);
 void e_exit(int code, const char *format, ...);
 int main(int argc, char *argv[]);
 
+/**
+ * Elf headers
+ */
+
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
+
 #endif /**MAIN_H**/
 
