1618fbc00e46b7dcfcaa4d13d69a5af79dee186c
Roslyn10 <roslynclewis1004@gmail.com>
2023-12-07 23:17:05 +0200

    Adding 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,10 +3,30 @@
 /**
  * add_dnodeint - A function that adds a new node at the beginning of a list
  * Description - Adds a new node at the beginning of a list
- * @head:
- * @n:
- * Return:
+ * @head: Head of the struct
+ * @n: Integer value
+ * Return: Address of the new element or NULL if failed
  */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	dlistint_t *newNode = malloc(sizeof(dlistint_t));
+
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+
+	newNode->n = n;
+	newNode->next = *head;
+	newNode->prev = NULL;
+
+	if (*head != NULL)
+	{
+		(*head)->prev = newNode;
+	}
+
+	*head = newNode;
+
+	return (newNode);
+}
