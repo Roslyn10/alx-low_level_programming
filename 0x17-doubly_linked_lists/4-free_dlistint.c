ce681367b9bce98e73dd80579f5fc41a8d5b1490
Roslyn10 <roslynclewis1004@gmail.com>
2023-12-07 23:32:47 +0200

    Freeing

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -3,9 +3,19 @@
 /**
  * free_dlistint - A function that frees a list
  * Description - Frees a list
- * @head:
- * Return: 
+ * @head: Head pointer
+ * Return: Nothing
  */
 
 void free_dlistint(dlistint_t *head)
 {
+	dlistint_t *current = head;
+	dlistint_t *newNode;
+
+	while (current != NULL)
+	{
+		newNode = current->next;
+		free(current);
+		current = newNode;
+	}
+}
