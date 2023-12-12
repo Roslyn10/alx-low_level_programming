0ca1c110499973eb23e93d4faab8d1afb1438d22
Roslyn10 <roslynclewis1004@gmail.com>
2023-12-12 09:59:15 +0200

    Fixing header file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
+#include "main.h" 
 /**
- * print_times_tables - Prints the times table
+ * print_times_table - Prints the times table
  * Description - Prints the times table of a given number
  * @n: Give number
  * Return: 0 Always(Success)
  */
-void print_times_tables(int n)
+
+void print_times_table(int n)
 {
-	if (n < 10)
-	{
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n + '0');
-	}
-	else if (n < 100)
-	{
-		_putchar(' ');
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else
-	{
-		_putchar((n / 100) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
-	}
+    if (n < 10)
+    {
+        _putchar(' ');
+        _putchar(' ');
+        _putchar(n + '0');
+    }
+    else if (n < 100)
+    {
+        _putchar(' ');
+        _putchar((n / 10) + '0');
+        _putchar((n % 10) + '0');
+    }
+    else
+    {
+        _putchar((n / 100) + '0');
+        _putchar(((n / 10) % 10) + '0');
+        _putchar((n % 10) + '0');
+	_putchar('\n');
+    }
 }
