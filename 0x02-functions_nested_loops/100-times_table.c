a84ccd2332aa50bdeba606fff0b92b6da750ed59
Roslyn10 <roslynclewis1004@gmail.com>
2023-12-12 10:19:44 +0200

    Adding times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,24 +8,45 @@
  */
 
 void print_times_table(int n)
-{
-	if (n < 10)
+{ int i, x, z;
+
+	for (x = 0; x < (n + 1); x++)
 	{
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n + '0');
-	}
-	else if (n < 100)
-	{
-		_putchar(' ');
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else
-	{
-		_putchar((n / 100) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
+		if (n < 0 || n > 15)
+			break;
+		_putchar(0 + '0');
+		if (n == 0)
+		{	_putchar('\n');
+			break;
+		}
+		_putchar(',');
+		for (i = 1; i < (n + 1); i++)
+		{	z = x * i;
+			if (z > 99)
+			{
+				_putchar(' ');
+				_putchar((z / 100) + '0');
+				_putchar(((z / 10) % 10) + '0');
+				_putchar((z % 10) + '0');
+			}
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((z % 10) + '0');
+			}
+			if (i == n)
+				break;
+			_putchar(',');
+		}
 		_putchar('\n');
 	}
 }
