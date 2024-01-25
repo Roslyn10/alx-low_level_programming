2e6d5ed8678d6cdef143f1bac6526cf605bc7f24
Roslyn10 <roslynclewis1004@gmail.com>
2024-01-25 14:19:25 +0200

    Adding Other files

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * *insert_dnodeint_at_index - A function that inserts a new node at a given position
+ * Description - Inserts a new node at a given position
+ * @h:
+ * @idx:
+ * @n:
+ * Return:
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+
+}
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - A function that deletes the node at index index of a linked list
+ * Description - Deletes a node at the index index of a list
+ * @head:
+ * @index:
+ * Return:
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+
+}
