91f242d07b85cf5ffb74ebde44487928ee133eee
Roslyn10 <roslynclewis1004@gmail.com>
2024-02-16 14:47:46 +0200

    Adding the test cases and coding files for 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - A fuction that deletes a hash table
+ * Description - Deletes a hash table
+ * @ht: The hash table thats going to be deleted
+ * Return: Nothing
+ */
+
+void hash_table_delete(hash_table_t *ht)
