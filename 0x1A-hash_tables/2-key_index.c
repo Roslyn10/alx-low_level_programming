1eeb4c7f15b1dc6168b770ab8490241f02a4fff4
Roslyn10 <roslynclewis1004@gmail.com>
2024-02-16 14:30:16 +0200

    Adding test case file and coding file for 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - A function that gives you the index of a key
+ * Description - Gives you the index of a key
+ * @key: The key
+ * @size: The size of the array of the hash table
+ * Return:
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
