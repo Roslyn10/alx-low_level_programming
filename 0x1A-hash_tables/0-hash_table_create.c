3a39969250880a4545ab0efcce767952f9434f94
Roslyn10 <roslynclewis1004@gmail.com>
2024-02-16 14:00:45 +0200

    Adding the testcase file for 0 and coding file

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - A function that creates a hash table
+ * Description - Creates a hash table
+ * @size:
+ * Return: A pointer to the newly created hash table/ 
+ * Null is something went wrong
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
