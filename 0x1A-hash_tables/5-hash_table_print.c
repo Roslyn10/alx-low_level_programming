1dfaf6696df67a57494a63d75e3fd2b3e55678fe
Roslyn10 <roslynclewis1004@gmail.com>
2024-02-16 14:44:53 +0200

    Adding test cases and coding files for 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - A function that prints a hash table
+ * Description - Prints a hash table
+ * @ht: The hash table
+ * Return: 
+ */
+
+void hash_table_print(const hash_table_t *ht)
