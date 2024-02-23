116ce9ecbaf88c2ee45dc3a9153570e38aa950e6
Roslyn10 <roslynclewis1004@gmail.com>
2024-02-23 19:09:41 +0200

    Adding the code for file 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,9 +3,19 @@
 /**
  * dlistint_len - A function that returns the number of elements in a list
  * Description - Returns the number of elements in a list
- * @h:
- * Return:
+ * @h: Head of the list
+ * Return: Number of nodes in the list
  */
 
 size_t dlistint_len(const dlistint_t *h)
 {
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		node_count++;
+	}
+
+	return (node_count);
+}
