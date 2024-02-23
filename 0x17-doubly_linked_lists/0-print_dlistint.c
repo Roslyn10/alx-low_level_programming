f2f79cd8192039b5c4229131cbb19c62db80456b
Roslyn10 <roslynclewis1004@gmail.com>
2024-02-23 18:44:10 +0200

    Adding code for file 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,9 +3,25 @@
 /**
  * print_dlistint - A function that prints all the elements of alist
  * Description - Prints all the elements of a list
- * @h:
- * Return:
+ * @h: Head of the list
+ * Return: The number of nodes
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
+	const dlistint_t *current = h;
+	size_t node_count = 0;
+
+	if (h == NULL)
+	{
+		return (node_count);
+	}
+
+	while (current != NULL)
+	{
+		current = current->next;
+		node_count++;
+	}
+
+	return (node_count);
+}
