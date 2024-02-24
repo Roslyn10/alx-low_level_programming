0334b8631757004c4eda916b234b09acd31ae47a
Roslyn10 <roslynclewis1004@gmail.com>
2024-02-24 16:10:58 +0200

    Adding code for file 4 and fixing betty

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -3,9 +3,18 @@
 /**
  * free_dlistint - A function that frees a list
  * Description - Frees a list
- * @head:
+ * @head: Pointer to the head of the list
  * Return: Nothing
  */
 
 void free_dlistint(dlistint_t *head)
 {
+	dlistint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
