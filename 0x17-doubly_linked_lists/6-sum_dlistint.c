30e9ffb33a45bea6380e8370494f26839597cfdc
Roslyn10 <roslynclewis1004@gmail.com>
2024-05-14 10:57:52 +0200

    Adding the code for 9 sum dlistint

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -3,9 +3,20 @@
 /**
  * sum_dlistint - A function that returns the sum of all the data of a list
  * Description - Returns the sum of all the data of a list
- * @head:
- * Return:
+ * @head: The pointer to the head of the list
+ * Return: 0 if the list is empty
  */
 
 int sum_dlistint(dlistint_t *head)
 {
+	int sum = 0;
+	dlistint_t *current = head;
+
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+
+	return (sum);
+}
