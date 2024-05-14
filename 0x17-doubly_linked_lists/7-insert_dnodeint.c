246986b6700f9a5fb6d084b00373e673c5558241
Roslyn10 <roslynclewis1004@gmail.com>
2024-05-14 11:22:31 +0200

    Adding the code for 8

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,3 +11,50 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *newNode = createNode(n);
+	dlistint_t *current = *h;
+	unsigned int count = 0;
+
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+
+	if (*h == NULL && idx != 0)
+	{
+		return (NULL);
+	}
+
+	if (idx == 0)
+	{
+		newNode->next = *h;
+		if (*h != NULL)
+		{
+			(*h)->prev = newNode;
+		}
+		*h = newNode;
+		return (newNode);
+	}
+
+	while (current != NULL & count < idx - 1)
+	{
+		current = current->next;
+		count++;
+	}
+
+	if (current == NULL)
+	{
+		free(newNode);
+		return (NULL);
+	}
+
+	newNode->next = current->next;
+	newNode->prev = current;
+	if (current->next != NULL)
+	{
+		current->next->prev = newNode;
+	}
+	current->next = newNode;
+
+	return (newNode);
+}
--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -3,10 +3,47 @@
 /**
  * delete_dnodeint_at_index - A function that deletes the node at index
  * Description - Deletes the node at index of a list
- * @head:
- * @index:
- * Return:
+ * @head: Pointer to the pointer to the head of the list
+ * @index: The position of the node
+ * Return: 1 if successful, -1 if failed
  */
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *current = *head;
+	unsigned int count = 0;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		if (*head != NULL)
+		{
+			(*head)->prev = NULL;
+		}
+		free(current);
+		return (1);
+	}
+	while (current != NULL && count < index)
+	{
+		current = current->next;
+		count++;
+	}
+	if (current == NULL)
+	{
+		return (-1);
+	}
+	if (current->prev != NULL)
+	{
+		current->prev->next = current->next;
+	}
+	if (current->next != NULL)
+	{
+		current->next->prev = current->prev;
+	}
+	free(current);
+	return (1);
+}
