62e6d6396649d9d701f139eb673e8694e7d0bc21
Roslyn10 <roslynclewis1004@gmail.com>
2024-05-21 11:42:38 +0200

    Adding the file for binary search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,16 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - A function that searchs for a value using binary search
+ * Description - Searches for a value in sorted array of integers...
+ * ...using the Binary search algorithm
+ * @array:
+ * @size:
+ * @value:
+ * Return:
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+
+}
