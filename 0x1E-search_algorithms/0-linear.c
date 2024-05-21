32ac6d25b628d5b0a952f8e947d91fa0cdb47b8e
Roslyn10 <roslynclewis1004@gmail.com>
2024-05-21 11:39:15 +0200

    Adding the file for linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,15 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - A function that searchers for a value using linear search
+ * Description - Searches for a value in an array using linear search algorithm
+ * @array:
+ * @size:
+ * @value:
+ * Return:
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+
+}
