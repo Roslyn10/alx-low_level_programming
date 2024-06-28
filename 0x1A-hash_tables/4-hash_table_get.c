639a9c74ac9ca41b63a1a0ddd433f5900cb1b0dd
Roslyn10 <roslynclewis1004@gmail.com>
2024-06-28 15:57:49 +0200

    Adding the code for 4, retrieve value with a key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,3 +9,27 @@
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (!ht || !key)
+	{
+		return (NULL);
+	}
+
+	index = hash_djb2((const unsigned char *)key) % ht->size;
+	node = ht->array[index];
+
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			return (node->value);
+		}
+		node = node->next;
+	}
+
+	return (NULL);
+}
+				
