78308bdb2032f7813e00bfb261439e4a4edc46a0
Roslyn10 <roslynclewis1004@gmail.com>
2024-06-28 16:26:40 +0200

    Fixing the code for 5, adding space between the list

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -26,7 +26,7 @@ void hash_table_print(const hash_table_t *ht)
 			while (temp)
 			{
 				if (c)
-					printf(",");
+					printf(", ");
 				printf("'%s': '%s'", temp->key, temp->value);
 				temp = temp->next;
 				c = 1;
