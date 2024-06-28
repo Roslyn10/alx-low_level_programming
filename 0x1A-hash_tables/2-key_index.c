aeae2e3a8241674a68680b8668f3c75a98458051
Roslyn10 <roslynclewis1004@gmail.com>
2024-06-28 15:31:38 +0200

    Adding the code for 2, key index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -5,7 +5,14 @@
  * Description - Gives you the index of a key
  * @key: The key
  * @size: The size of the array of the hash table
- * Return:
+ * Return: The key/value pair at index
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	index = hash_djb2(key);
+
+	return (index % size);
+}
